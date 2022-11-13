aa29fa2966792ca9e0efee42acacf84b0e588a07
JP171001 <josepa1710@gmail.com>
2022-11-12 21:42:48 -0600

    comienza el black and white

--- proyecto02/src/fsstructure/save_disk.rs
+++ proyecto02/src/fsstructure/save_disk.rs
@@ -1,13 +1,13 @@
 use fuse::{Filesystem, Request, ReplyCreate, ReplyEmpty, ReplyAttr, ReplyEntry, ReplyOpen, ReplyStatfs,ReplyData, ReplyDirectory, ReplyWrite, FileType, FileAttr};
 use libc::{ENOSYS, ENOENT, EIO, EISDIR, ENOSPC};
 use std::ffi::OsStr;
+use std::io::Cursor;
 use std::mem;
 use serde::{Serialize, Deserialize};
 use crate::fileAttribute;
-use image::Luma;
+use image::{Pixel, Luma, GenericImageView, GenericImage, DynamicImage, ImageBuffer, Rgb, Rgba, RgbaImage, RgbImage, GrayImage, GrayAlphaImage, ImageOutputFormat};
 use crate::Disk::*;
-
-
+use image::io::Reader as ImageReader;
 #[path = "src/fsstructure/Disk.rs"] use Disk;
 
 
@@ -23,6 +23,67 @@ pub fn decode(object: Vec<u8>) -> Disk {
     let decoded: Disk = bincode::deserialize(&object[..]).unwrap();
     return decoded;
 }
+
+// =================================================================================================
+// No se descarta, pueden salir ideas de acá
+// =================================================================================================
+/*
+//Escribe pixeles en una imagen
+pub fn write_pixels(height: u32, width: u32 , data: Vec<u8>) {
+    let mut imgbuf = image::ImageBuffer::new(width as usize as u32, height as u32);
+    let mut i = 0;
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        if data[i] == 0 {
+            *pixel = image::Luma([0]);
+            i = i + 1;
+        } else {
+            *pixel = image::Luma([255]);
+            i = i + 1;
+        }
+    }
+    imgbuf.save("test.png").unwrap();
+}
+*/
+
+
+
+// =================================================================================================
+// falta, crear la imagen de una vez con el tamaño correcto *
+// probar en el main ***
+// En teoría funciona el for y el if, ya más de 2 fuentes lo hacen parecido
+// https://docs.rs/image/latest/image/
+// https://blog.logrocket.com/decoding-encoding-images-rust-using-image-crate/?ref=morioh.com&utm_source=morioh.com
+// https://morioh.com/p/a3e5136ef8db
+// =================================================================================================
+//Escribe pixeles en una imagen
+pub fn write_pixels(height: u32, width: u32, data: Vec<u8>) {
+
+    let mut img = image::open("image.png").unwrap();
+    //let (width, height) = img.dimensions();
+
+    let mut output = ImageBuffer::new(width, height);
+    let (width, height) = output.dimensions();
+
+    let mut i = 0;
+    for (x, y, pixel) in img.pixels() {
+        if data[i] == 0 {
+            output.put_pixel(x, y,
+                             // 0 is black, 255 is white
+                             pixel.map(|p| p.saturating_sub(0))
+            );
+            i = i + 1;
+        } else {
+            output.put_pixel(x, y,
+                                // 0 is black, 255 is white
+                             pixel.map(|p| p.saturating_sub(255))
+            );
+            i = i + 1;
+        }
+
+    }
+
+}
+
 
 pub fn validate_path(path:String) -> bool{
     let img = image::open(path);
